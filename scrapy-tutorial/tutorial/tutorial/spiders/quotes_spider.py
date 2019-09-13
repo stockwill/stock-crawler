45f9de53f25528554a4be88e4ca8d40ae22136a2
stockwill <stockvolition@gmail.com>
2019-09-13 21:23:14 +0800

    Extract quotes to json

--- scrapy-tutorial/tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy-tutorial/tutorial/tutorial/spiders/quotes_spider.py
@@ -9,15 +9,10 @@ class QuotesSpider(scrapy.Spider):
         ]
 
     def parse(self, response):
-        # self.log('Current url: %s' % response.url)
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-
         for quote in response.css("div.quote"):
             text = quote.css("span.text::text").get()
             author = quote.css("small.author::text").get()
             tags = quote.css("div.tags a.tag::text").getall()
-            print(dict(text=text, author=author, tags=tags))
+            yield {
+                'text': text, 'author': author, 'tags': tags
+            }
