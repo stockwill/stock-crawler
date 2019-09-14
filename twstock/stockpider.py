0fabb9aa382ee0e0de3ab72e63aa44927fccb67a
stockwill <stockvolition@gmail.com>
2019-09-14 16:17:04 +0800

    Disobey robots.txt

--- twstock/stockpider.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-class StockSpider(scrapy.Spider):
-    name = 'stockspider'
-    start_urls = ['https://mops.twse.com.tw/mops/web/t146sb05']
-
-    def parse(self, response):
-        pass
